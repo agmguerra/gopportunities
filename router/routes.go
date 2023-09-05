6ccda71018ca8e5f2b1febf035e1f02cbf54f7ff
Alexandre Guerra <agmguerra@gmail.com>
2023-09-05 08:34:55 -0300

    feat: ajustes

--- router/routes.go
+++ router/routes.go
@@ -8,7 +8,7 @@ import (
 func initializeRoutes(router *gin.Engine) {
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/openings", handler.ShowOpeningHandler)
+		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
 		v1.POST("/opening", handler.UpdateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
