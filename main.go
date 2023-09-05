3961ab23a1168c82cf48cfe4c8df0ce803a2ac66
Alexandre Guerra <agmguerra@gmail.com>
2023-09-05 00:59:49 -0300

    feat: adding an applicaation logger

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"github.com/agmguerra/gopportunities/config"
 	"github.com/agmguerra/gopportunities/router"
 )
 
+var (
+	logger config.Logger
+)
+
 func main() {
+
+	logger = *config.GetLogger("main")
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config init error: %v", err)
+		return
+	}
 
 	// Initialize Router
 	router.Initialize()
