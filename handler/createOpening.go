2e786c6eb25dfb89b8a2b712b8aac655981b023d
Alexandre Guerra <agmguerra@gmail.com>
2023-09-06 00:58:02 -0300

    feat: create opening logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,11 +3,40 @@ package handler
 import (
 	"net/http"
 
+	"github.com/agmguerra/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
+	request := CreateOpeningRequest{}
+
+	// bind json request
+	if err := ctx.BindJSON(&request); err != nil {
+		logger.Errorf("json bind error: %v", err)
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// validate fields
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	// create opening
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+		sendError(ctx, http.StatusUnprocessableEntity, err.Error())
+		return
+	}
+	sendSuccess(ctx, "create-opening", opening)
 }
--- router/routes.go
+++ router/routes.go
@@ -6,11 +6,14 @@ import (
 )
 
 func initializeRoutes(router *gin.Engine) {
+	//Initilize handler
+	handler.InitializeHandler()
+
 	v1 := router.Group("/api/v1")
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.GET("/openings", handler.ListOpeningsHandler)
-		v1.POST("/opening", handler.UpdateOpeningHandler)
+		v1.POST("/opening", handler.CreateOpeningHandler)
 		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 	}
